8585fa54b151316f0ceeedb4c7229dc9f84a235c
Bryan Lucas <lbryan235@gmail.com>
2024-09-25 09:27:12 -0300

    Feito pequenas correçoes no código

--- quinto.py
+++ quinto.py
@@ -3,12 +3,12 @@ n2 = input ("Digite a segunda a nota do aluno\n")
 n3 = input ("Digite a terceira nota do aluno\n")
 n4 = input ("Digite a quarta nota do aluno\n")
 
-n1 = float(n1)
-n2 = float(n2)
-n3 = float(n3)
-n4 = float(n4)
+n1 = float (n1)
+n2 = float (n2)
+n3 = float (n3)
+n4 = float (n4)
 
-média = n1 + n2 + n3 + n4 / 4
+média = (n1 + n2 + n3 + n4) / 4
 
 if média >= 6:
     print("Aprovado")
--- sexto.py
+++ sexto.py
@@ -1,7 +1,7 @@
 numero_placa = input ("Digite o número final da placa de seu veículo:\n")
 
 match numero_placa:
-    case "1""2": print("Rodizio na Segunda-feira")
+    case "1"|"2": print("Rodizio na Segunda-feira")
     case "3"|"4": print("Rodizio na Terça-feira")
     case "5"|"6": print("Rodizio na Quarta-feira")
     case "7"|"8": print("Rodizio na Quinta-feira")
